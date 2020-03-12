ac0701316c85dac4c4f2e4de2cbc50e066643209
Nasim Hayath <nhayath@gmail.com>
2020-03-12 13:57:56 +0000

    add server

--- shipitfile.js
+++ shipitfile.js
@@ -28,5 +28,59 @@ module.exports = shipit => {
       'shared',
       'ecosystem.config.js'
     );
+
+    // Our listeners and tasks will go here
+    shipit.on('updated', async () => {
+        // one update fire these 2 events
+        shipit.start('npm-install', 'copy-config');
+    });
+
+    shipit.on('published', async () => {
+        shipit.start('pm2-server');
+    });
+
+
+    // on event `copy-config` do this blocking task
+    shipit.blTask('copy-config', async () => {
+        const fs = require('fs');
+        const ecosystem = `
+        module.exports = {
+            apps: [
+                {
+                name: '${appName}',
+                script: '${shipit.releasePath}/server.js',
+                watch: true,
+                autorestart: true,
+                restart_delay: 1000,
+                env: {
+                    NODE_ENV: 'development'
+                },
+                env_production: {
+                    NODE_ENV: 'production'
+                }
+                }
+            ]
+        };`;
+
+        fs.writeFileSync('ecosystem.config.js', ecosystem, function(err) {
+            if (err) throw err;
+            console.log('File created successfully.');
+          });
+      
+          await shipit.copyToRemote('ecosystem.config.js', ecosystemFilePath);
+    });
+
+    shipit.blTask('npm-install', async () => {
+        shipit.remote(`cd ${shipit.releasePath} && npm install --production`);
+    });
+
+    shipit.blTask('pm2-server', async () => {
+        await shipit.remote(`pm2 delete -s ${appName} || :`);
+        await shipit.remote(
+            `pm2 start ${ecosystemFilePath} --env production --watch true`
+        );
+    });
+
+
   
   };
